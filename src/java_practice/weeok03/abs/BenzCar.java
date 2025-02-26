3f4d0e093599b1ec2d2dbc9577d37ca9accb851f
ji-0o0o0o0 <tlswldud328@gmail.com>
2025-02-26 23:42:26 +0900

    250226 다형성, 상속, 추상클래스, 인터페이스

---
+++ src/java_practice/weeok03/abs/BenzCar.java
@@ -0,0 +1,8 @@
+package java_practice.weeok03.abs;
+public class BenzCar extends Car {
+
+    @Override
+    public void horn() {
+        System.out.println("Benz 빵빵");
+    }
+}
---
+++ src/java_practice/weeok03/abs/GenesisCar.java
@@ -0,0 +1,8 @@
+package java_practice.weeok03.abs;
+public class GenesisCar extends Car {
+
+    @Override
+    public void horn() {
+        System.out.println("Zenesis 빵빵");
+    }
+}
